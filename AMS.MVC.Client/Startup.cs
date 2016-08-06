4b5f1298400baa931721afe34f8b12237bcc6fba
SRABAN-CST <nadimcst75@gmail.com>
2016-08-06 18:30:39 +0600

    Add project

---
+++ AMS.MVC.Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AMS.MVC.Client.Startup))]
+namespace AMS.MVC.Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ AMS.Models/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Details { get; set; }
+        public string Origin { get; set; }
+        public int Quantity { get; set; }
+    }
+}
